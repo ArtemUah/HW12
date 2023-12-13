aab79cdf22274289f980e3984c55d530607392e1
Artem <111749288+ArtemUah@users.noreply.github.com>
2023-12-13 19:43:14 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -70,3 +70,78 @@
 // };
 
 // fetch(BASE_URL, options).then(resp => resp.json()).then(data => console.log(data));
+
+// -----------Weather in capital of countries
+// 'https://restcountries.com/v3.1/name/'
+//  const BASE_URL = "http://api.weatherapi.com/v1";
+// const API_KEY = "ce2cb9b2a3da414bb5b172546231704";
+
+const countriesApi = 'https://restcountries.com/v3.1/name/'
+
+const searchForm = document.querySelector('.js-searchForm');
+const inputCountriesWrapper = document.querySelector('.js-wrapper');
+const addBtn = document.querySelector('.js-add');
+const list = document.querySelector('.js-list')
+addBtn.addEventListener('click', handlerOnClick);
+function handlerOnClick(e){
+inputCountriesWrapper.insertAdjacentHTML('beforeend', createMarkup())
+}
+
+function createMarkup (){
+    return `<input name="country" type="text">`
+}
+
+searchForm.addEventListener('submit', handlerOnSubmit);
+async function handlerOnSubmit (e){
+    e.preventDefault();
+    const data = new FormData(searchForm);
+    const countries = data.getAll('country');
+    const checkedCountries = countries.map(country => country.trim()).filter(country => country);
+    getCapitals(checkedCountries).then(async data => {
+        const resps = await getWeather(data);
+        const weather = resps.filter(item => item.status === 'fulfilled').map(item => item.value);
+        
+            list.innerHTML = createWeatherMarkup(weather);
+            searchForm.innerHTML='';
+    })
+};
+
+async function getCapitals (arr){
+const resps = arr.map(async country => {
+const resp = await fetch(`${countriesApi}${country}`);
+if(!resp.ok){
+    throw new Error()
+};
+return resp.json()
+});
+const data = await Promise.allSettled(resps);
+const countryObj = data.filter(item => item.status === 'fulfilled').map(country => country.value[0]);
+return countryObj;
+}
+
+async function getWeather (arr) {
+     const BASE_URL = "http://api.weatherapi.com/v1/current.json";
+const API_KEY = "ce2cb9b2a3da414bb5b172546231704";
+    const resps = arr.map(async country => {
+        const resp = await fetch(`${BASE_URL}?key=${API_KEY}&q=${country.capital[0]}`);
+        if (!resp.ok){
+            throw new Error()
+        };
+        return resp.json();
+    });
+    const data = await Promise.allSettled(resps);
+    const weatherObj = data.filter(item => item.status === 'fulfilled');
+    return weatherObj;
+}
+
+function createWeatherMarkup(arr){
+    return arr.map(({location:{country, name}, current:{temp_c, condition:{text, icon}}}) => {
+        return `<li>
+        <img src="${icon}" alt="${name}">
+        <h2>${name}</h2>
+        <h3>${country}</h3>
+        <p>${text}</p>
+        <p>${temp_c}</p>
+      </li>`
+    }).join('')
+};
